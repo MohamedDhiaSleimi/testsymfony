b90947ac7c008542db99659964022cf94be8cd16
Moahmed Dhia Sleimi <sleimi.mohameddhia@gmail.com>
2025-04-30 12:10:57 +0100

    third commit

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -13,14 +13,13 @@ use App\Entity\Recette;
 
 class RecetteController extends AbstractController {
     private string $nom = 'Cake au citron';
-    private int $type = 1;
+    private int $type = 2;
     // 1 ( sucré ), 2 ( salé )
     private string $description = 'Facile à préparer avec des ingrédients du quotidien';
     private array $ingredients = [ 'Farine', 'Sucre', 'Oeufs', 'Citron', 'Beurre' ];
     private array $quantites = [ '200g', '100g', '3', '1', '100g' ];
 
     #[ Route( '/affichage', name: 'affichage' ) ]
-
     public function afficher(): Response {
         return $this->render( 'recette/affiche.html.twig', [
             'nom' => $this->nom,
@@ -30,7 +29,6 @@ class RecetteController extends AbstractController {
     }
 
     #[ Route( '/details', name: 'details' ) ]
-
     public function details(): Response {
         return $this->render( 'recette/details.html.twig', [
             'nom' => $this->nom,
@@ -38,8 +36,9 @@ class RecetteController extends AbstractController {
             'quantites' => $this->quantites,
         ] );
     }
-    #[ Route( '/ajout', name: 'recette_ajout' ) ]
 
+    
+    #[ Route( '/ajout', name: 'recette_ajout' ) ]
     public function ajouter( Request $request, EntityManagerInterface $em ): Response {
         $recette = new Recette();
         $form = $this->createForm( RecetteType::class, $recette );
@@ -51,14 +50,14 @@ class RecetteController extends AbstractController {
             return $this->redirectToRoute( 'recette_liste' );
         }
 
-        return $this->render( 'ajout.html.twig', [
+        return $this->render( 'recette/ajout.html.twig', [
             'form' => $form->createView(),
         ] );
     }
     #[Route('/detail/{id}', name: 'recette_detail')]
     public function detail(Recette $recette): Response
     {
-        return $this->render('detail.html.twig', [
+        return $this->render('recette/detail.html.twig', [
             'recette' => $recette,
         ]);
     }
@@ -71,10 +70,12 @@ class RecetteController extends AbstractController {
         return $this->redirectToRoute('recette_liste');
     }
     
+   
+
     #[ Route( '/liste', name: 'recette_liste' ) ]
     public function lister( RecetteRepository $recetteRepo ): Response {
         $recettes = $recetteRepo->findAll();
-        return $this->render( 'liste.html.twig', [
+        return $this->render( 'recette/liste.html.twig', [
             'recettes' => $recettes,
         ] );
     }
